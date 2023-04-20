24003333425cb5ec9696607e2555731d863cbc2f
maurohcardona <maurohcardona@gmail.com>
2023-04-19 22:07:40 -0300

    casi todo
    falta ver errores

---
+++ 1-Pre-Entrega/src/Routs/carts.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const CartManager = require('../cartManager')
+const cart = new CartManager()
+const ProductManager = require('../productmanager')
+const producto = new ProductManager()
+
+const routerCarts = express.Router()
+
+routerCarts.post('/', (req, res) => {
+    cart.addCart()
+    res.status(200).send('Cart added successfully')
+})
+
+routerCarts.get('/:cid', async(req, res) => {   
+    const cid = parseInt(req.params.cid)
+    const viewCart = await cart.path.find(e => e.id === cid)
+    if(!viewCart){
+        res.status(404).end('Cart not found')
+    }else{
+        res.status(200).send({cart: viewCart})
+    }
+})
+
+routerCarts.post('/:cid/product/:pid', async (req, res) => {
+    const cid = parseInt(req.params.cid)
+    const pid = parseInt(req.params.pid)
+    cart.addProductToCart(cid, pid)
+    const viewCart = await cart.path.find(e => e.id === cid)
+    const viewProduct = await producto.path.find(e => e.id === pid)
+    if (!viewCart) {
+        res.status(404).end('the cart doesnt exist')
+    }else if (!viewProduct) {
+        res.status(404).end('The product doesnt exist')
+    }else{
+        res.status(200).send({
+            Mesaje: 'Product added successfully',
+            cart: viewCart
+        })
+    }
+})
+
+module.exports = routerCarts
--- 1-Pre-Entrega/src/app.js
+++ 1-Pre-Entrega/src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const routerProducts = require('./Routs/products.js')
+const routerCarts = require('./Routs/carts.js')
 const PORT = 8080
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true } )) 
 
 app.use('/api/products', routerProducts)
+app.use('/api/carts', routerCarts)
 
 app.get('/api/', (req, res) => {
     res.send('Node curse');
