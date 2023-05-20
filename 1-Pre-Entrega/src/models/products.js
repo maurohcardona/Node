4de7d89a752a3990a12ee816d9e4363db737624c
maurohcardona <maurohcardona@gmail.com>
2023-05-19 21:22:50 -0300

    Hola

---
+++ 1-Pre-Entrega/src/models/products.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const productCollection = 'products';
+
+const productSchema = new mongoose.Schema({
+        "title": "Alhohada",
+        "description": "Almohada",
+        "price": "500",
+        "status": "",
+        "thumbnail": "eeke",
+        "code": "393948",
+        "stock": "29",
+        "category": "Ropa de cama"
+
+})
