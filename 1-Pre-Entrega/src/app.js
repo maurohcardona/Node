747b5ce03427b53da9a8ceb0b5d4e93ff27b78ab
maurohcardona <maurohcardona@gmail.com>
2023-05-23 19:55:11 -0300

    ordenando

--- 1-Pre-Entrega/src/app.js
+++ 1-Pre-Entrega/src/app.js
@@ -2,8 +2,8 @@ import  express  from 'express';
 const app = express();
 import path from 'path';
 import productManager from './dao/Controllers/productmanager.js';
-//const routerProducts = require('./Routs/products.js')
-//import routerProducts from './Routs/products.js';
+
+
 //const routerCarts = require('./Routs/carts.js')
 //import routerCarts from './Routs/carts.js';
 //const routerRealTimeProducts = require('./Routs/realTimeProducts.js')
@@ -12,6 +12,7 @@ const PORT = 8080
 import __dirname from './utils.js';
 import productRouter from './dao/Routs/products.js';
 import cartRouter from './dao/Routs/carts.js';
+import messageManager from './dao/Controllers/messagemanager.js';
 //const { Server } = require('socket.io')
 import { Server } from 'socket.io'
 //const ProductManager = require('./productmanager')
@@ -20,6 +21,8 @@ import { Server } from 'socket.io'
 
 import { engine }  from "express-handlebars";
 import routerCarts from './Routs/carts.js';
+import messageRouter from './dao/Routs/messages.js';
+
 
 const publics = path.join(__dirname, './public');
 
@@ -33,6 +36,7 @@ app.use(express.urlencoded({ extended: true } ))
 
 app.use('/', productRouter)
 app.use('/', cartRouter)
+app.use('/', messageRouter);
 //app.use('/api/carts', routerCarts)
 //app.use ('/realtimeproducts', routerRealTimeProducts )
 //app.use(express.static(__dirname + '/Public'))
@@ -45,23 +49,20 @@ const httpServer = app.listen(PORT, () =>{
     console.log(`listening on port ${PORT}`);
 })
 
-// const socketServer = new Server(httpServer)
+const io = new Server(httpServer)
 
-// socketServer.on('connection',async (socket) => {
-//     console.log('New connection')   
 
-//     socket.on('newProduct',async data => {
-//         const ProductManager = new productManager();
-//        await ProductManager.createProduct(data);
-//         const productos = await ProductManager.getProduct()
-//         socket.emit('sendProducts', productos);
-//         console.log(productos);
-//     })
+io.on('connection', socket => {
+    console.log('New connection')   
 
-    // socket.on('deleteProduct', id => {
-    //     producto.deleteProduct(id)
-    //     socket.emit('senddelete', producto.path.filter (p => p.id !== id))
-    // })
+    socket.on('message', async data => {
+        const MessageManager  = new messageManager();
+        await MessageManager.createMessage(data);
+        const mensajes = await MessageManager.getMessage()
+        io.emit('messages', mensajes);
+        console.log(mensajes);
+    })
+
     
-//})
+})
 
